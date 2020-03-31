0103923f19d8d1b8517384b204db6034ab12fcbe
BrandonKutivan <bskutivan@gmail.com>
2020-03-31 16:13:55 -0700

    included task-type to taskHandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,20 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var CreateTaskHandler = function() {
 
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name = 'task-type']").value;
+    console.dir(taskTypeInput);
+    // Create List Item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info"
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemE1.appendChild(taskInfoE1);
+    // Add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
