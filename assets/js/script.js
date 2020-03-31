9126ab0c55a8848ce3e7703d18c1987430e8072d
BrandonKutivan <bskutivan@gmail.com>
2020-03-30 22:33:44 -0700

    set up javascript to create a task

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+console.dir(window.document);
+
+var buttonE1 = document.querySelector("#save-task");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+var CreateTaskHandler = function() {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
+buttonE1.addEventListener("click", CreateTaskHandler);
+
