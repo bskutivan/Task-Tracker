e54ac1e0bd74f98fe36c6f26627b820f5ba6679c
BrandonKutivan <bskutivan@gmail.com>
2020-03-31 15:46:07 -0700

    adjusted eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 console.dir(window.document);
 
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var CreateTaskHandler = function() {
+
+    event.preventDefault();
+    
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", CreateTaskHandler);
+formE1.addEventListener("submit", CreateTaskHandler);
 
