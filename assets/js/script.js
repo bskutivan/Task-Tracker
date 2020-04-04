d36eb5d5614473a30f21fad43967fc671cdba06c
BrandonKutivan <bskutivan@gmail.com>
2020-04-04 13:28:29 -0700

    refactored loadTasks functions and called back createTasksE1 function since it had the same code

--- assets/js/script.js
+++ assets/js/script.js
@@ -269,45 +269,16 @@ var saveTasks = function() {
 }
 
 var loadTasks = function() {
-    tasks = localStorage.getItem("tasks", tasks);
+    var savedTasks = localStorage.getItem("tasks", tasks);
 
-    if (!tasks) {
-        tasks = [];
+    if (!savedTasks) {
         return false;
     };
 
-    tasks = JSON.parse(tasks);
+    savedTasks = JSON.parse(savedTasks);
 
-    for (var i = 0; i < tasks.length; i++) {
-        tasks[i].id = taskIdCounter;
-        // create list element
-        var listItemE1 = document.createElement("li");
-        listItemE1.className = "task-item";
-        listItemE1.setAttribute("data-task-id", tasks[i].id);
-        listItemE1.setAttribute("draggable", "true");
-
-        // create div element
-        var taskInfoE1 = document.createElement("div");
-        taskInfoE1.className = "task-info";
-        taskInfoE1.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-
-        listItemE1.appendChild(taskInfoE1);
-
-        var taskActionsE1 = createTaskActions(tasks[i].id);
-        listItemE1.appendChild(taskActionsE1);
-
-        if (tasks[i].status === "to do") {
-            listItemE1.querySelector("select[name='status-change']").selectedIndex = 0
-            tasksToDoE1.appendChild(listItemE1);
-        } else if (tasks[i].status === "in progress") {
-            listItemE1.querySelector("select[name='status-change']").selectedIndex = 1
-            tasksInProgressE1.appendChild(listItemE1);
-        } else if (tasks[i].status === "complete") {
-            listItemE1.querySelector("select[name='status-change']").selectedIndex = 2
-            tasksCompletedE1.appendChild(listItemE1);
-        }
-        taskIdCounter++
-        console.log(listItemE1)
+    for (var i = 0; i < savedTasks.length; i++) {
+        createTaskE1(savedTasks[i]);
     }
 }
 
